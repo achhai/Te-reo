6de3d606ee24dc2394ab85e6c7331f93bacc2c32
achhai <101298315+achhai@users.noreply.github.com>
2022-06-07 10:33:21 +1200

    Update main v1.py

--- main v1.py
+++ main v1.py
@@ -1,6 +1,5 @@
 # Ask player if they wanna start the game
 response1 = input("Hello, welcome to Te Reo Quiz game! would you like to start? ")
-score = 0
 
 # Every correct question the player answer gives 1 score
 # Question 1
@@ -15,7 +14,6 @@ else:
 
 if play1 == "4" or play1 == "kia ora":
   print ("Correct")
-  score += 1
 
 
 elif play1 == "1" or play1 == "ciao" or play1 == "2" or play1 == "privet" or play1 == "3" or play1 == "salut":
